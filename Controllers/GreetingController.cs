de6e8b50d1a50535a379fefbe295886a42bdd6dc
Anant Chauhan <anantchauhan003@gmail.com>
2024-01-31 15:28:42 -0500

    Add project files.

---
+++ Controllers/GreetingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace assignment1.Controllers
+{
+    public class GreetingController : ApiController
+    { public string Post()
+        {
+            return "hello world";
+        }
+      public string Get(int id)
+        {
+            return $"greeting to {id} people";
+        }
+    }
+}
---
+++ Controllers/NumberMachineController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace assignment1.Controllers
+{
+    public class NumberMachineController : ApiController
+    { public int Get(int id)
+        {
+            return id + 10 - 2 * 5 / 4; 
+        }
+    }
+}
